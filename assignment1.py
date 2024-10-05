c0dc82b8023b9c5e63ccfa1db62828c53815fc17
ruoyu <709421127@qq.com>
2024-10-05 19:41:24 +0930

    split dataset

--- assignment1.py
+++ assignment1.py
@@ -1,8 +1,14 @@
-
 from sklearn.datasets import load_svmlight_file
+from sklearn.model_selection import train_test_split
 #load dataset
 X, y = load_svmlight_file('diabetes_scale')
 X = X.toarray()
-print(X.shape)
-print(y.shape)
-print(y)
+
+#split dataset
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+print(X_train.shape)
+print(X_test.shape)
+print(y_train.shape)
+print(y_test.shape)
+
+  
