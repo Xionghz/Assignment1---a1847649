ff71f420702815def672361e2acd1c8e75e24997
ruoyu <709421127@qq.com>
2024-10-06 15:28:01 +1030

    perceptron implement

--- assignment1.py
+++ assignment1.py
@@ -1,14 +1,76 @@
 from sklearn.datasets import load_svmlight_file
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+import numpy as np
 #load dataset
 X, y = load_svmlight_file('diabetes_scale')
 X = X.toarray()
 
 #split dataset
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-print(X_train.shape)
-print(X_test.shape)
-print(y_train.shape)
-print(y_test.shape)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-  
+#perceptron class
+class Perceptron:
+    def __init__(self, max_iter=440, step_size=0.0001):
+        self.max_iter = max_iter
+        self.step_size = step_size
+        self.accuracy = []
+        self.train_accuracy = []
+        
+
+    def fit(self, X, y):
+        #initialize weight to 1
+        self.w = np.zeros(X.shape[1])
+        #initialize bias to 0
+        self.b = 0
+        #loop through the dataset for max_iter times
+        for i in range(self.max_iter):
+            #test if all samples are correctly classified
+            #loop through each sample in the dataset
+            for j in range(X.shape[0]):
+
+                # if y*(wx+b) <= 0, update weights and bias
+                if y[j] * (np.dot(X[j], self.w)+self.b) <= 0:
+
+
+                    self.w += self.step_size * y[j] * X[j]
+                    self.b += self.step_size * y[j]
+            #calculate accuracy
+            y_pred = perceptron.predict(X_test)
+            correct = 0
+            for j in range(len(y_test)):
+                if y_test[j] == y_pred[j]:
+                    correct += 1
+            accuracy = correct / len(y_test)
+            self.accuracy.append(accuracy)
+            #calculate training accuracy
+            y_pred = perceptron.predict(X_train)
+            correct = 0
+            for j in range(len(y_train)):
+                if y_train[j] == y_pred[j]:
+                    correct += 1
+            accuracy = correct / len(y_train)
+            self.train_accuracy.append(accuracy)
+           
+            
+    
+        return self
+
+    def predict(self, X):
+        predict_y =  np.sign(np.dot(X, self.w)+self.b)
+        return predict_y
+    
+
+#train perceptron
+perceptron = Perceptron()
+perceptron.fit(X_train, y_train)
+print (perceptron.accuracy[-1])
+#plot accuracys
+plt.plot(perceptron.accuracy)
+plt.plot(perceptron.train_accuracy)
+plt.show()
+
+
+
+ 
